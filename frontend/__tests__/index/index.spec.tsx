2977f8f8d880d1e485fdc834f51336ba678686a8
Princh Canal <princhcanal@gmail.com>
2021-11-04 12:24:53 +0800

    fix(fe test): fix landing page test

--- frontend/__tests__/index/index.spec.tsx
+++ frontend/__tests__/index/index.spec.tsx
@@ -8,7 +8,7 @@ describe('Home Page', () => {
 
   it('shows landing page', () => {
     const heading = screen.getByRole<HTMLHeadingElement>('heading', {
-      name: /handijob/i,
+      name: 'HandiJob',
     });
 
     const loginLink = screen.getByRole<HTMLAnchorElement>('link', {
