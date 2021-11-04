803580b49d3b90681400f86a4f8eb2ad25b4902e
Dina <dinagenedelatorre@gmail.com>
2021-11-04 18:22:36 +0800

    feat(landing): HNDJB-10 dina first pr

--- frontend/src/modules/index/components/Team/teamMembers.ts
+++ frontend/src/modules/index/components/Team/teamMembers.ts
@@ -1,3 +1,10 @@
 import { TeamMember } from './TeamMember/TeamMember';
 
-export const teamMembers: TeamMember[] = [];
+const dina: TeamMember = {
+  name: 'Ma. Dina Gene S. Dela Torre',
+  role: 'Developer and Product Owner',
+  imageUrl:
+    'https://scontent.fceb2-1.fna.fbcdn.net/v/t1.6435-9/84189333_4058323397527037_7186479514644381696_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=e3f864&_nc_eui2=AeEKaltmcp7vgyfXbFYpKuQjkstIh7UAFBGSy0iHtQAUER-2P_7fhxC1TIGavQXOjiazWV3feJbDWPT1yW1E-Zou&_nc_ohc=COFNNhYeoJMAX8sLRvC&_nc_ht=scontent.fceb2-1.fna&oh=3c3e3673cedefea14108c8bc270372a8&oe=61A8EF8F',
+};
+
+export const teamMembers: TeamMember[] = [dina];
