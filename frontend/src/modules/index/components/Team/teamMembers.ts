25b70d49fbfb06d7e3c816eaafae4748d77040b0
Dina <dinagenedelatorre@gmail.com>
2021-11-04 18:58:14 +0800

    feat(landing): HNDJB-12 ej first pr

--- frontend/src/modules/index/components/Team/teamMembers.ts
+++ frontend/src/modules/index/components/Team/teamMembers.ts
@@ -1,5 +1,12 @@
 import { TeamMember } from './TeamMember/TeamMember';
 
+const ej: TeamMember = {
+  name: 'Edsel John Tabra',
+  role: 'Product Tester',
+  imageUrl:
+    'https://scontent.fceb2-1.fna.fbcdn.net/v/t39.30808-6/242984887_6131400186931119_735161883704037550_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeHdZjbJJ0uTai7eVtY0qzTagJaRhvrY9lOAlpGG-tj2UzBv1ks01sN-BR9Q30TM9Ik94dJnSP827Y1ZuxFMyvQg&_nc_ohc=dRP8BeeGvKsAX9bObcM&_nc_oc=AQkw34hxCGVhf2KAcT0xaIRidX1zFU-4gt8imu6O2nA9DfWD_InvU6J9FN8sBtgXmuo2Bmu3lqovVsHzdNmvGaxS&tn=g8ODrEV8JuKAD_SK&_nc_ht=scontent.fceb2-1.fna&oh=7a440ef503ec0bb52e5e93ed8e565ac9&oe=6187D632',
+};
+
 const dina: TeamMember = {
   name: 'Ma. Dina Gene S. Dela Torre',
   role: 'Developer and Product Owner',
@@ -7,4 +14,4 @@ const dina: TeamMember = {
     'https://scontent.fceb2-1.fna.fbcdn.net/v/t1.6435-9/84189333_4058323397527037_7186479514644381696_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=e3f864&_nc_eui2=AeEKaltmcp7vgyfXbFYpKuQjkstIh7UAFBGSy0iHtQAUER-2P_7fhxC1TIGavQXOjiazWV3feJbDWPT1yW1E-Zou&_nc_ohc=COFNNhYeoJMAX8sLRvC&_nc_ht=scontent.fceb2-1.fna&oh=3c3e3673cedefea14108c8bc270372a8&oe=61A8EF8F',
 };
 
-export const teamMembers: TeamMember[] = [dina];
+export const teamMembers: TeamMember[] = [ej, dina];
