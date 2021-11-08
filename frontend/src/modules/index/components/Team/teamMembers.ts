325e5aff7f826dd5b2150cbeb9778b934c7db234
Princh Canal <princhcanal@gmail.com>
2021-11-08 15:56:30 +0800

    fix(landing): replace expired link for ej

--- frontend/src/modules/index/components/Team/teamMembers.ts
+++ frontend/src/modules/index/components/Team/teamMembers.ts
@@ -4,7 +4,7 @@ const ej: TeamMember = {
   name: 'Edsel John Tabra',
   role: 'Product Tester',
   imageUrl:
-    'https://scontent.fceb2-1.fna.fbcdn.net/v/t39.30808-6/242984887_6131400186931119_735161883704037550_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeHdZjbJJ0uTai7eVtY0qzTagJaRhvrY9lOAlpGG-tj2UzBv1ks01sN-BR9Q30TM9Ik94dJnSP827Y1ZuxFMyvQg&_nc_ohc=dRP8BeeGvKsAX9bObcM&_nc_oc=AQkw34hxCGVhf2KAcT0xaIRidX1zFU-4gt8imu6O2nA9DfWD_InvU6J9FN8sBtgXmuo2Bmu3lqovVsHzdNmvGaxS&tn=g8ODrEV8JuKAD_SK&_nc_ht=scontent.fceb2-1.fna&oh=7a440ef503ec0bb52e5e93ed8e565ac9&oe=6187D632',
+    'https://scontent.fceb1-2.fna.fbcdn.net/v/t39.30808-6/242984887_6131400186931119_735161883704037550_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=m9lHvGbu2MQAX_U3FVF&_nc_ht=scontent.fceb1-2.fna&oh=4d66c7684085836ee000cca407e5deab&oe=618DC4F2',
 };
 
 const dina: TeamMember = {
