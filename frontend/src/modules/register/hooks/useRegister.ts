91a21530744bb807f60cb3557337fc3da2e5134d
Princh Canal <princhcanal@gmail.com>
2021-11-16 12:42:39 +0800

    fix(auth): fix register flow

--- frontend/src/modules/register/hooks/useRegister.ts
+++ frontend/src/modules/register/hooks/useRegister.ts
@@ -2,14 +2,14 @@ import { useMutation } from 'react-query';
 import { RegisterUserDTO } from 'generated-api';
 import { useContext } from 'react';
 import { ApiContext } from '../../../shared/providers/ApiProvider';
-// import { useRouter } from 'next/router';
+import { useRouter } from 'next/router';
 import { useGlobalStore } from '../../../shared/stores';
 import { LocalStorageKeys } from '../../../shared/enums/localStorageKeys';
 
 export const useRegister = () => {
   const api = useContext(ApiContext);
   const setUser = useGlobalStore((state) => state.setUser);
-  // const router = useRouter();
+  const router = useRouter();
 
   return useMutation(
     (registerDTO: RegisterUserDTO) => api.register(registerDTO),
@@ -21,8 +21,7 @@ export const useRegister = () => {
         localStorage.setItem(LocalStorageKeys.ACCESS_TOKEN, accessToken);
         localStorage.setItem(LocalStorageKeys.REFRESH_TOKEN, refreshToken);
 
-        // router.push('/');
-        window.location.href = '/';
+        router.push('/');
       },
     }
   );
