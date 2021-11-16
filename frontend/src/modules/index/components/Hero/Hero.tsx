713e959641efae942d9db82dc9b930195911abd5
Princh Canal <princhcanal@gmail.com>
2021-11-16 17:59:00 +0800

    feat(hero): add get started button

--- frontend/src/modules/index/components/Hero/Hero.tsx
+++ frontend/src/modules/index/components/Hero/Hero.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from './Hero.module.scss';
+import NextLink from 'next/link';
+import { Box, Link } from '@chakra-ui/react';
 
 export const Hero = () => {
   return (
@@ -7,9 +9,16 @@ export const Hero = () => {
       <div className={styles.left}>
         <h1 className={styles.heading}>Apply to HandIJob!</h1>
         <p>Take your career further with HandIJob</p>
+        <Box mt='10'>
+          <NextLink href='/register' passHref>
+            <Link bg='teal.500' color='teal.50' py='4' px='8' rounded='full'>
+              Get Started
+            </Link>
+          </NextLink>
+        </Box>
       </div>
       <div className={styles.right}>
-        <img src='/landing.svg' alt='' />
+        <img src='/landing.svg' alt='landing' />
       </div>
     </div>
   );
