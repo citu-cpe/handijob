dce8852158fb9e494aa032175bb692adde24ffd2
Princh Canal <princhcanal@gmail.com>
2021-11-17 16:34:00 +0800

    fix(auth): fix logout flow

--- frontend/src/modules/index/hooks/useLogout.ts
+++ frontend/src/modules/index/hooks/useLogout.ts
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router';
 import { useContext } from 'react';
 import { useMutation } from 'react-query';
 import { ApiContext } from '../../../shared/providers/ApiProvider';
@@ -7,10 +8,12 @@ export const useLogout = () => {
   const api = useContext(ApiContext);
   const user = useGlobalStore((state) => state.user);
   const logoutUser = useGlobalStore((state) => state.logoutUser);
+  const router = useRouter();
 
   return useMutation(() => api.logOut(user!), {
     onSuccess: () => {
       logoutUser();
+      router.push('/');
     },
   });
 };
