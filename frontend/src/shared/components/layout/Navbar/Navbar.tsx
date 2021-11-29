6bb7557390b32068da3805f590506c08a12d9154
Princh Canal <princhcanal@gmail.com>
2021-11-29 15:46:03 +0800

    fix e2e test

--- frontend/src/shared/components/layout/Navbar/Navbar.tsx
+++ frontend/src/shared/components/layout/Navbar/Navbar.tsx
@@ -1,25 +1,8 @@
-import {
-  Text,
-  Flex,
-  Box,
-  Link,
-  Container,
-  Button,
-  Img,
-  IconButton,
-  Popover,
-  PopoverArrow,
-  PopoverBody,
-  PopoverContent,
-  PopoverHeader,
-  PopoverTrigger,
-} from '@chakra-ui/react';
+import { Flex, Box, Link, Container, Button, Img } from '@chakra-ui/react';
 import React from 'react';
 import NextLink from 'next/link';
 import { useLogout } from '../../../../modules/index/hooks/useLogout';
-import { BellIcon } from '@chakra-ui/icons';
 import { Notifications } from '../Notifications/Notifications';
-import { useGetNotifications } from '../../../hooks/useGetNotifications';
 
 interface NavbarProps {
   positionFixed?: boolean;
@@ -28,8 +11,6 @@ interface NavbarProps {
 
 export const Navbar = ({ positionFixed, loggedIn }: NavbarProps) => {
   const { mutate: logout, isLoading: logoutIsLoading } = useLogout();
-  const { notificationsDTO, isLoading: notificationsIsLoading } =
-    useGetNotifications();
 
   return (
     <Box
@@ -54,53 +35,7 @@ export const Navbar = ({ positionFixed, loggedIn }: NavbarProps) => {
           <Box>
             {loggedIn ? (
               <>
-                <Popover>
-                  <PopoverTrigger>
-                    <Box pos='relative' display='inline'>
-                      <IconButton
-                        colorScheme='teal'
-                        icon={<BellIcon />}
-                        aria-label='notifications'
-                        variant='ghost'
-                        size='lg'
-                        mr='4'
-                      />
-                      {notificationsDTO && notificationsDTO.numUnread > 0 && (
-                        <Flex
-                          justifyContent='center'
-                          alignItems='center'
-                          display='inline-flex'
-                          pos='absolute'
-                          top='-12px'
-                          right='15px'
-                          bgColor='red'
-                          color='white'
-                          rounded='full'
-                          h='5'
-                          w='5'
-                          textAlign='center'
-                          p='2'
-                          cursor='pointer'
-                          fontSize='xs'
-                        >
-                          <Text>{notificationsDTO?.numUnread}</Text>
-                        </Flex>
-                      )}
-                    </Box>
-                  </PopoverTrigger>
-                  <PopoverContent>
-                    <PopoverArrow />
-                    <PopoverHeader color='teal' fontWeight='bold'>
-                      Notifications
-                    </PopoverHeader>
-                    <PopoverBody p='0'>
-                      <Notifications
-                        notifications={notificationsDTO?.notifications || []}
-                        isLoading={notificationsIsLoading}
-                      />
-                    </PopoverBody>
-                  </PopoverContent>
-                </Popover>
+                <Notifications />
                 <Button
                   mr='4'
                   colorScheme='teal'
