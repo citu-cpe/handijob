a2e2718ab0a1b84703d54ded68d624778a8d5bd1
Princh Canal <princhcanal@gmail.com>
2021-12-12 20:32:20 +0800

    feat(filters): HNDJB-40 filter job openings by category

--- frontend/src/modules/index/components/Home/Home.tsx
+++ frontend/src/modules/index/components/Home/Home.tsx
@@ -6,16 +6,25 @@ import {
   ModalContent,
   ModalHeader,
   ModalOverlay,
+  Text,
   useDisclosure,
+  Checkbox,
+  CheckboxGroup,
+  Wrap,
 } from '@chakra-ui/react';
 import { AddIcon } from '@chakra-ui/icons';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BottomRightButton } from '../../../../shared/components/button/BottomRightButton';
-import { UserDTO } from 'generated-api';
+import {
+  JobOpeningDTO,
+  JobOpeningDTOCategoriesEnum,
+  UserDTO,
+} from 'generated-api';
 import { useGetJobOpenings } from './hooks/useGetJobOpenings';
 import { JobOpening } from './components/JobOpening/JobOpening';
 import { JobOpeningForm } from './components/JobOpeningForm/JobOpeningForm';
 import { JobOpeningSkeleton } from './components/JobOpeningSkeleton/JobOpeningSkeleton';
+import { useGetCategories } from './components/JobOpeningForm/hooks/useGetCategories';
 
 interface HomeProps {
   user: UserDTO;
@@ -24,11 +33,48 @@ interface HomeProps {
 export const Home = ({ user }: HomeProps) => {
   const { jobOpenings, isLoading } = useGetJobOpenings();
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const { categories } = useGetCategories();
+  const [filters, setFilters] = useState<JobOpeningDTOCategoriesEnum[]>([]);
+  const [filteredJobOpenings, setFilteredJobOpenings] = useState<
+    JobOpeningDTO[]
+  >([]);
+
+  useEffect(() => {
+    setFilteredJobOpenings(getFilteredJobOpenings(jobOpenings, filters));
+  }, [jobOpenings, filters]);
+
+  const getFilteredJobOpenings = (
+    originalJobOpenings: JobOpeningDTO[],
+    newFilters: JobOpeningDTOCategoriesEnum[]
+  ) => {
+    if (newFilters.length === 0) { return originalJobOpenings; }
+
+    return originalJobOpenings.filter((j) =>
+      j.categories.some((c) => newFilters.includes(c))
+    );
+  };
 
   return (
     <Box>
+      <Box rounded='md' shadow='md' bgColor='white' p='4' mb='4'>
+        <Text mb='2' fontWeight='bold'>
+          Fitler by category:
+        </Text>
+        <CheckboxGroup
+          colorScheme='teal'
+          onChange={(val: JobOpeningDTOCategoriesEnum[]) => setFilters(val)}
+        >
+          <Wrap spacing='4'>
+            {categories.map((category) => (
+              <Checkbox key={category.name} value={category.name}>
+                {category.name}
+              </Checkbox>
+            ))}
+          </Wrap>
+        </CheckboxGroup>
+      </Box>
       {isLoading && <JobOpeningSkeleton />}
-      {jobOpenings.map((jobOpening) => (
+      {filteredJobOpenings.map((jobOpening) => (
         <JobOpening key={jobOpening.id} jobOpening={jobOpening} />
       ))}
 
